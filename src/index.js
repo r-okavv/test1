72facbeb132523e379b1358908e5f16f26ee67c7
r-okavv <101883340+r-okavv@users.noreply.github.com>
2022-08-23 06:15:21 +0900

    add js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+const onClickaAdd = () => {
+  //テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickaAdd());
